f11fdcf9a46013ad7c12413623678f71b96c86c4
Volodymyr M. Lisivka <vlisivka@gmail.com>
2026-04-19 09:29:23 +0300

    Color fix

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,9 +4,9 @@
 set -ueE -o pipefail
 
 # Colors for output
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+GREEN=$'\033[0;32m'
+RED=$'\033[0;31m'
+NC=$'\033[0m' # No Color
 
 panic() {
     echo "${RED}ERROR:${NC} $*" >&2
